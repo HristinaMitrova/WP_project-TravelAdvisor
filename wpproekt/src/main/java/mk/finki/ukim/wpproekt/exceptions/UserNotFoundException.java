e33e3af7f13a4bda7d1eae9d0e6e823be96a70bf
Marija <mitrovahristina11@gmail.com>
2020-05-12 23:16:15 +0200

    Upgrade no.3

--- wpproekt/src/main/java/mk/finki/ukim/wpproekt/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mk.finki.ukim.wpproekt.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    UserNotFoundException(Long id) {
-        super("Could not find user with ID: " + id);
-    }
-}
